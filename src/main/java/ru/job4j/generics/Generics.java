c2654bfdd887e87da31936131086ae8fa676e47d
Dmitry Marakov <dmarakov@devexperts.com>
2023-03-26 15:03:00 +0400

    Fixed parameter for printObject

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -29,8 +29,8 @@ public class Generics {
         /*  gen.printLowerBoundedWildCard(third); */
     }
 
-    public void printObject(List<? extends Animal> list) {
-        for (Iterator<? extends Animal> it = list.iterator(); it.hasNext();) {
+    public void printObject(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext();) {
             Object next = it.next();
             System.out.println("Текущий элемент: " + next);
         }
