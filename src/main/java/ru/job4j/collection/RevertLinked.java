f5ee643d8466a6ddb2be7ad88f486b4a04746eec
Dmitry Marakov <dmarakov@devexperts.com>
2023-03-30 02:29:07 +0400

    6. Перевернуть связанный список [#161]

---
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -0,0 +1,113 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class RevertLinked<T> implements Iterable<T> {
+
+    private int size = 0;
+    private int modCount = 0;
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> f = head;
+        if (f == null) {
+            head = new Node<>(value, null);
+        } else {
+            while (f.next != null) {
+                f = f.next;
+            }
+            f.next = new Node<>(value, null);
+        }
+        size++;
+        modCount++;
+    }
+
+    public void addFirst(T value) {
+        head = new Node<>(value, head);
+        size++;
+        modCount++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> rsl = head;
+        for (int i = 0; i < index; i++) {
+            rsl = rsl.next;
+        }
+        return rsl.item;
+    }
+
+
+    public T deleteFirst() {
+        if (size <= 0) {
+            throw new NoSuchElementException();
+        }
+        T rsl = head.item;
+        Node<T> tmp = head.next;
+        head.next = null;
+        head.item = null;
+        head = tmp;
+        size--;
+        modCount++;
+        return rsl;
+    }
+
+    public boolean revert() {
+        var rsl = false;
+        if (size > 1) {
+            Node<T> current = head;
+            Node<T> prev;
+            Node<T> next = head.next;
+            while (next != null) {
+                prev = current;
+                current = next;
+                next = current.next;
+                current.next = prev;
+
+            }
+            head.next = null;
+            head = current;
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> point = head;
+            final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T element = point.item;
+                point = point.next;
+                return element;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
