58c805d1f56a34d2996ae2027e200fcb2c70b3ad
Dmitry Marakov <dmarakov@devexperts.com>
2023-03-31 03:50:00 +0400

    - Fix vision scope of variables - add one more condition to check

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -6,12 +6,12 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
-    int inSize = 0;
-    int outSize = 0;
+    private int inSize = 0;
+    private int outSize = 0;
 
 
     public T poll() {
-        if (outSize < 0) {
+        if (outSize < 0 || inSize < 0) {
             throw new NoSuchElementException();
         }
         if (outSize == 0) {
