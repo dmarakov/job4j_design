81c31b19ee9262842c20a50b6eb6577958052a34
Dmitry Marakov <dmarakov@devexperts.com>
2023-04-03 02:11:47 +0400

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,53 @@
+package ru.job4j.set;
+
+import ru.job4j.collection.SimpleArrayList;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class SimpleSet<T> implements Set<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
+
+    @Override
+    public boolean add(T value) {
+        boolean rsl = false;
+        if (set.size() == 0) {
+            set.add(value);
+            rsl = true;
+        } else {
+            if (!contains(value)) {
+                set.add(value);
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        return Objects.equals(set.iterator().next(), value);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SimpleSet<?> simpleSet = (SimpleSet<?>) o;
+        return Objects.equals(set, simpleSet.set);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(set);
+    }
+}
---
+++ src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.set;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SimpleSetTest {
+
+    @Test
+    void whenAddNonNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+    }
+
+    @Test
+    void whenAddNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(null)).isTrue();
+        assertThat(set.contains(null)).isTrue();
+        assertThat(set.add(null)).isFalse();
+    }
+
+    @Test
+    void whenNotContains() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(2)).isTrue();
+        assertThat(set.contains(3)).isFalse();
+    }
+
+    @Test
+    void whenGetIteratorFromEmptyListThenNextThrowException() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThatThrownBy(set.iterator()::next)
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}
