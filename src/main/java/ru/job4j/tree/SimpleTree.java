a0ddf2cadcc83c0ed5760d3ad3ee04d33a1af30f
Dmitry Marakov <dmarakov@devexperts.com>
2023-04-16 05:28:32 +0400

    Fix add method

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -13,15 +13,9 @@ public class SimpleTree<E> implements Tree<E> {
     public boolean add(E parent, E child) {
         boolean rsl = false;
         Optional<Node<E>> parentNode = findBy(parent);
-        if (parentNode.isPresent()) {
-            if (root.children.size() != 0) {
-                for (Node<E> eNode : root.children) {
-                    if (eNode.value == child) {
-                        return false;
-                    }
-                }
-            }
-            root.children.add(new Node<>(child));
+        Optional<Node<E>> childNode = findBy(child);
+        if (parentNode.isPresent() && childNode.isEmpty()) {
+            parentNode.get().children.add(new Node<>(child));
             rsl = true;
         }
         return rsl;
@@ -36,6 +30,21 @@ public class SimpleTree<E> implements Tree<E> {
             Node<E> el = data.poll();
             if (el.value.equals(value)) {
                 rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+
+    public boolean isBinary() {
+        boolean rsl = true;
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.children.size() > 2) {
+                rsl = false;
                 break;
             }
             data.addAll(el.children);
--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -2,6 +2,8 @@ package ru.job4j.tree;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTreeTest {
 
@@ -32,5 +34,30 @@ public class SimpleTreeTest {
         tree.add(4, 5);
         tree.add(5, 6);
         assertThat(tree.add(2, 6)).isFalse();
+    }
+
+    @Test
+    public void testIsBinaryWhenTreeIsBinary() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
+        assertTrue(tree.add(2, 4));
+        assertTrue(tree.add(2, 5));
+        assertTrue(tree.add(3, 6));
+        assertTrue(tree.add(3, 7));
+        assertTrue(tree.isBinary());
+    }
+
+    @Test
+    public void testIsBinaryWhenTreeIsNotBinary() {
+        SimpleTree<Integer> tree = new SimpleTree<>(1);
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
+        assertTrue(tree.add(2, 4));
+        assertTrue(tree.add(2, 5));
+        assertTrue(tree.add(3, 6));
+        assertTrue(tree.add(3, 7));
+        assertTrue(tree.add(3, 8));
+        assertFalse(tree.isBinary());
     }
 }
