a4581fc7a0b4161d2f9a1daba31631013a4ed924
Dmitry Marakov <dmarakov@devexperts.com>
2023-04-17 00:10:53 +0400

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,28 @@
+package ru.job4j.question;
+
+import java.util.*;
+
+public class Analize {
+
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        Map<Integer, String> map = new HashMap<>();
+        Info info = new Info(0, 0, 0);
+        for (User user : previous) {
+            map.put(user.getId(), user.getName());
+        }
+        for (User user : current) {
+            if (!map.containsKey(user.getId())) {
+                info.setAdded(info.getAdded() + 1);
+            }
+            if (map.containsKey(user.getId()) && !map.containsValue(user.getName())) {
+                info.setChanged(info.getChanged() + 1);
+            }
+            map.remove(user.getId());
+
+        }
+        info.setDeleted(map.size());
+        return info;
+    }
+
+}
