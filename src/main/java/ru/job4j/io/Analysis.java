439ff7828f2440bed1b83af1a18347e5c29dd7db
Dmitry Marakov <dmarakov@devexperts.com>
2023-04-23 02:11:34 +0400

    Fixed if condition

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,25 +5,16 @@ import java.io.*;
 public class Analysis {
     public void unavailable(String source, String target) {
         boolean isUnavailable = false;
-        StringBuilder sb = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             while (in.ready()) {
                 String line = in.readLine();
                 String[] splitLine = line.split(" ");
-                if (!isUnavailable && ("400".equals(splitLine[0]) || "500".equals(splitLine[0]))) {
-                    isUnavailable = true;
-                    sb.append(splitLine[1]).append(";");
-                } else if (isUnavailable && !("400".equals(splitLine[0]) || "500".equals(splitLine[0]))) {
-                    isUnavailable = false;
-                    sb.append(splitLine[1]).append(";").append(System.lineSeparator());
-                }
-                try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-                    out.append(sb);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (("400".equals(splitLine[0]) || "500".equals(splitLine[0])) != isUnavailable) {
+                    isUnavailable = !isUnavailable;
+                    out.write(splitLine[1] + (isUnavailable ? ";" : ";\n"));
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
