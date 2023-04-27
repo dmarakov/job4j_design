4ee0c4ffb3f78030cfc62c3c6fcf72b313bd5de9
Dmitry Marakov <dmarakov@devexperts.com>
2023-04-27 04:01:21 +0400

    Moved validation to dedicated method

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -16,34 +16,34 @@ public class ArgsName {
 
     private void parse(String[] args) {
         for (String arg : args) {
+            validateArg(arg);
             String[] splitedArg = arg.split("=", 2);
             values.put(splitedArg[0].substring(1), splitedArg[1]);
         }
     }
 
     public static ArgsName of(String[] args) {
-
         if (args.length == 0) {
             throw new IllegalArgumentException("Arguments not passed to program");
-        }
-        for (String arg : args) {
-            String[] splitedArg = arg.split("=", 2);
-            if (!arg.contains("=")) {
-                throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain an equal sign", arg));
-            }
-            if (!arg.contains("-")) {
-                throw new IllegalArgumentException(String.format("Error: This argument '%s' does not start with a '-' character", arg));
-            }
-            if (splitedArg[0].startsWith("-") && splitedArg[0].length() == 1) {
-                throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain a key", arg));
-            }
-            if (splitedArg[1].isEmpty()) {
-                throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain a value", arg));
-            }
         }
         ArgsName names = new ArgsName();
         names.parse(args);
         return names;
+    }
+
+    public static void validateArg(String arg) {
+        if (!arg.contains("=")) {
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain an equal sign", arg));
+        }
+        if (!arg.startsWith("-")) {
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not start with a '-' character", arg));
+        }
+        if (arg.startsWith("-") && arg.split("=")[0].length() == 1) {
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain a key", arg));
+        }
+        if (arg.substring(arg.indexOf("=") + 1).isEmpty()) {
+            throw new IllegalArgumentException(String.format("Error: This argument '%s' does not contain a value", arg));
+        }
     }
 
     public static void main(String[] args) {
