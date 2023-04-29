79885bc7da9034195473337a26365f9451560023
dmarakov <silencesewer@gmail.com>
2023-04-29 15:14:40 +0400

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -10,39 +11,49 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public void packFiles(List<File> sources, File target) {
-        for (File source : sources) {
-            try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File source : sources) {
                 zip.putNextEntry(new ZipEntry(source.getPath()));
                 try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
                     zip.write(out.readAllBytes());
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public static void validation(String directory, String exclude, String output) {
+        if (!(Files.exists(Path.of(directory)))) {
+            throw new IllegalArgumentException(String.format("Root folder '%s' is null. Usage ROOT_FOLDER.", directory));
+        }
+        if (!Files.isDirectory(Path.of(directory))) {
+            throw new IllegalArgumentException(String.format("'%s' is not a folder. Create a folder", directory));
+        }
+        if (!(exclude.startsWith(".") && exclude.length() > 1)) {
+            throw new IllegalArgumentException(String.format("File extension '%s' don't not specified. Use program arguments. It should start with '.' and has more than 1 symbol", exclude));
+        }
+        if (!(output.endsWith(".zip"))) {
+            throw new IllegalArgumentException(String.format("Your file %s have to has .zip extension", output));
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("The number of parameters passed must be 3");
+        }
         ArgsName argsName = ArgsName.of(args);
-        Search.validation(args);
-        List<Path> path = Search.search(Paths.get(argsName.get("d")), it -> it != Paths.get(argsName.get("e")));
+        String directory = argsName.get("d");
+        String exclude = argsName.get("e");
+        String output = argsName.get("o");
+        validation(directory, exclude, output);
+        List<Path> path = Search.search(Paths.get(directory), it -> !it.toFile().getName().endsWith(exclude));
         List<File> files = path.stream().map(Path::toFile).toList();
         System.out.println(path);
         Zip zip = new Zip();
         zip.packFiles(
                 files,
-                new File(argsName.get("o"))
+                new File("/Users/dmarakov/IdeaProjects/Test/" + output)
         );
     }
 }
